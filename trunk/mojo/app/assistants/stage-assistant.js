cec864d24ae2d9ce79997c6175b6f25673c89e14
thewbman <thewbman@2c35681a-fe91-b849-ad47-b4212db3104c>
2011-08-26 04:49:02 +0000

    Add help, preferences to Mojo version

--- trunk/mojo/app/assistants/stage-assistant.js
+++ trunk/mojo/app/assistants/stage-assistant.js
@@ -33,12 +33,10 @@ KTorrentTouch.appMenuModel = {
 	items: [
 		{label: $L("About"), command: 'do-aboutApp'},
 		{label: $L("Preferences"), command: 'do-prefsApp'},
-		{label: $L("Shortcuts"), items: [
-		
-			]
-		},
 		{label: $L("Help"), items: [
-		
+			{label: $L("Help"), command: 'do-openHelp'},
+			{label: $L("Leave review"), command: 'do-leaveReview'},
+			{label: $L("Email developer"), command: 'do-emailDeveloper'},
 			]
 		}
 	]
@@ -54,8 +52,90 @@ KTorrentTouch.cookieObject = KTorrentTouch.cookie.get();
 
 StageAssistant.prototype.setup = function() {
 
+	//Instantiate Metrix Library
+	KTorrentTouch.Metrix = new Metrix(); 
+	
 	//Start first scene
 	this.controller.pushScene("main");
 	this.controller.setWindowOrientation("up");
 
 };
+
+
+
+StageAssistant.prototype.handleCommand = function(event) {
+  var currentScene = this.controller.activeScene();
+  if(event.type == Mojo.Event.command) {
+    switch(event.command) {
+      case 'do-aboutApp':
+        
+			aboutinfo = "KTorrentTouch is an app for controlling a KTorrent program.  You should already have a KTorrent program setup and running on your computer for this app to work.   The torrents are downloaded to the computer and not to this device.  ";
+			
+			aboutinfo += '<hr/><center><a href="http://code.google.com/p/ktorrenttouch/">App homepage</a></center>';  
+			
+			//aboutinfo += '<hr/><a href="http://ktorrent.org/">KTorrent homepage</a>';  
+			
+			//aboutinfo += "Licensed under <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GPLv2</a>."
+
+            currentScene.showAlertDialog({
+                onChoose: function(value) {if (value==true) {
+					Mojo.Controller.stageController.pushScene("help");
+					}
+				},
+				title: Mojo.Controller.appInfo.title+" - v" + Mojo.Controller.appInfo.version,
+                //message: "Copyright 2011, Wes Brown <br />" + aboutinfo,
+                message: aboutinfo,
+                choices: [
+					{label: $L("OK"), value: false},
+					{label: $L("Help"), value: true}
+					],
+                allowHTMLMessage: true
+            });
+
+       break;
+	   
+	  case 'do-prefsApp':
+			Mojo.Controller.stageController.pushScene("preferences");
+       break;
+	   
+	  case 'do-openHelp':
+			Mojo.Controller.stageController.pushScene("help");
+       break;
+	   
+	  case 'do-leaveReview':
+			currentScene.serviceRequest("palm://com.palm.applicationManager", {
+				method: "open",
+				parameters:  {
+					id: 'com.palm.app.findapps',
+					params: {
+						scene: 'page',
+						target: "http://developer.palm.com/appredirect/?packageid=com.thewbman.ktorrenttouch"
+					}
+				}
+			});
+       break;
+	   
+	  case 'do-emailDeveloper':
+			currentScene.serviceRequest(
+				"palm://com.palm.applicationManager", {
+					method: 'open',
+                    parameters: {
+						id: "com.palm.app.email",
+                        params: {
+							summary: "Help with "+Mojo.Controller.appInfo.title+" v"+ Mojo.Controller.appInfo.version,
+                            recipients: [{
+								type:"email",
+                                value:"webmyth.help@gmail.com",
+                                contactDisplay:"KTorrentTouch Developer"
+                            }]
+                        }
+                    }
+                 }
+             );
+       break;
+	   
+
+    }
+  }
+};
+
